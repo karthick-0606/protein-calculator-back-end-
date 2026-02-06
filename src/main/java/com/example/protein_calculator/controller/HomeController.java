f13c416ae7114ae54d80426bcfcc7660b1909893
Karthick.S <23suca06@tcarts.in>
2026-02-06 14:39:48 +0530

    modified api

--- src/main/java/com/example/protein_calculator/controller/HomeController.java
+++ src/main/java/com/example/protein_calculator/controller/HomeController.java
@@ -1,9 +1,11 @@
 package com.example.protein_calculator.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "https://protein-calculator-front-end-7bi1.vercel.app")
 public class HomeController {
 
     @GetMapping("/")
